f9d8ac4baf358e2b98bbea8b8d478d306e52bab4
aman-kumar02 <amkumar.2002@gmail.com>
2026-09-20 12:12:55 +0530

    Testing jenkins trigger

--- src/main/java/com/learn/Homepage.java
+++ src/main/java/com/learn/Homepage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 public class Homepage extends BasePage{
 
     private final By PRODUCTS = By.xpath("//button[contains(text(),\"Products\")]");
+    private final By CLOSE_POP_UP = By.cssSelector("button[id=\"close-button\"]");
 
     public Homepage(WebDriver driver){
         super(driver);
@@ -17,6 +18,7 @@ public class Homepage extends BasePage{
     }
 
     public void exploreProducts(){
+        click(CLOSE_POP_UP);
         click(PRODUCTS);
     }
 }
